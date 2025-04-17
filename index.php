d8d934bc9a66d2519aa6bf2e36c326ee1a398aab
CARLOS MOLINA <camolina2024@incos-santacruz.edu.bo>
2025-04-17 13:22:46 -0400

    nvar

--- index.php
+++ index.php
@@ -14,22 +14,46 @@ include("db.php");
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body><!--grid system-->
-<!--cambios 2-->
-    <Section class="container">
-        <div class="row">
-            <!--columnas / numero de columnas a cubrir /
-            breakpoint sm md lg xxl-->
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <!--imag src"../IMAGENES/cliente.png"-->
-            </div>
-            <div class="col-sm-12  col-md-6 col-lg-8">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus animi facere doloremque eaque, aperiam culpa ipsa eligendi, rerum labore iure ratione pariatur quos et, nam aspernatur impedit eum quidem?</p>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
         </div>
-    </Section>
-    <!---<h1>BIENVENIDO</h1>
-    <a href="login.php">Login</a>*/-->
+    </nav>
+    </div>
 </body>
 
 </html>
